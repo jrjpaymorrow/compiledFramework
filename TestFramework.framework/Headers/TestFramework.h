d62e773f8d42674b8d9b6487c7df310811e9c473
Jan Rajtr <jrj@paymorrow.com>
2018-08-31 11:32:44 +0200

    prvni
    - first
    -

---
+++ TestFramework.framework/Headers/TestFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestFramework.h
+//  TestFramework
+//
+//  Created by Jan Rajtr on 31/08/2018.
+//  Copyright © 2018 Jan Rajtr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestFramework.
+FOUNDATION_EXPORT double TestFrameworkVersionNumber;
+
+//! Project version string for TestFramework.
+FOUNDATION_EXPORT const unsigned char TestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFramework/PublicHeader.h>
+
+
